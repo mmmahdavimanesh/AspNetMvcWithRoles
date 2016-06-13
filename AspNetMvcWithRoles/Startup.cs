7e66d92fdd1fd801e720ad0a7b5cc510ef5aa4eb
Xeronith <xeronith@yahoo.com>
2016-06-13 06:06:34 -0700

    Initial commit

---
+++ AspNetMvcWithRoles/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AspNetMvcWithRoles.Startup))]
+namespace AspNetMvcWithRoles
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
